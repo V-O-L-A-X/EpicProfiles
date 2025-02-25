ad608bc28986303baa4241e87fe197acbbd985d5
V-O-L-A-X <110328562+V-O-L-A-X@users.noreply.github.com>
2025-02-25 07:37:13 +0000

    .

--- XZ.php
+++ XZ.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>XZ</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body class="nen container ">
-    <h1 class="container alert alert-warning">Hello, world!</h1>
+<h1 class="container alert alert-warning">Hello, world!</h1>
     <?php include("dialogue.php"); ?>
     <div class="fs-3 fw-medium lh-base text-white">
     <p>
@@ -87,6 +77,3 @@
       a quick note on what you'd like to see changed or added to make it better in the future.
   </p>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
